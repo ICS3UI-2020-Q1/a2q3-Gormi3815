0b5cfea0571e9495719fbc09822d05638092b92d
Gormi3815 <gormi3815@wrdsb.ca>
2020-09-15 12:52:21 +0000

    made the program tell the user which of the two integers they put in would go first

--- Main.java
+++ Main.java
@@ -1,15 +1,32 @@
+import java.util.Scanner;
 /**
  *
- * @author 
+ * @author Isaac
  */
 public class Main {
 
-  /**
+  /** this program tells the user what order the numbers that they put in go first
    * The method that is executed when you hit the run button.
    * @param args the command line arguments
    */
   public static void main(String[] args) {
     // TODO code application logic here
-    
+    Scanner input = new Scanner(System.in);
+    System.out.println ("Please enter a number.");
+    int first_number = input.nextInt();
+    System.out.println ("please enter a second number.");
+    int second_number = input.nextInt();
+
+    if (first_number < second_number) {
+      System.out.println ("The order of these numbers are " + first_number + ", " + second_number + ".");
+    }
+    else if (first_number > second_number) {
+      System.out.println ("The order of these numbers are " + second_number + ", " + first_number + ".");
+    }
+    else if (first_number == second_number) {
+      System.out.println ("The order of these numbers are " + first_number + ", " + second_number + ".");
+    }
+
+
   }
 }
